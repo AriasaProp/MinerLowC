f0b6c4d5bae7cefac0ab15524d7c75b086b3c9c3
AriasaProp <ikomangwidiadaariasa12@gmail.com>
2025-08-10 15:04:37 +0800

    initialize

--- cmd.sh
+++ cmd.sh
@@ -10,13 +10,13 @@ fi
 
 case "$1" in
     "test")
-        echo "You do test."
+        echo "Do test."
         cmake . -DDO_TEST=1
         cmake --build . --config Debug
         ctest -V
         ;;
     "build")
-        echo "You do build main."
+        echo "Do build main."
         cmake .
         cmake --build .
         ;;
@@ -35,20 +35,16 @@ case "$1" in
           echo "This is currently not git repository. Return now"
           exit 1
         fi
+        # default commit message
+        commit_message="Update repository at $(date '+%x %R')"
         # if there is a second arguments
         if [ -n $2 ]; then
           commit_message=$2
-        else
-          # ask for comment
-          read -p "Commit message: " commit_message
-        fi
-        if [ -z $commit_message ]; then
-          commit_message="Update repository at $(date '+%x %R')"
         fi
         git add .
         git commit -m "$commit_message"
-        if [ $? -eq 0 ]; then
-          git push --set-upstream origin main
+        if [ -z $? ]; then
+          git push
         else
           echo "Nothing is changed."
           exit 1
